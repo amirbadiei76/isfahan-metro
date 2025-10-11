6968c6f75865d737c05a81ca54c46be11c65dbae
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-10-12 06:42:14 +1030

    App Created

---
+++ src/data/stations.ts
@@ -0,0 +1,28 @@
+export interface Station {
+    id: number;
+    name: string;
+}
+  
+
+export const stations: Station[] = [
+    { id: 1, name: 'قدس' },
+    { id: 2, name: 'بهارستان' },
+    { id: 3, name: 'مفتح' },
+    { id: 4, name: 'شهید علیخانی' },
+    { id: 5, name: 'جابر' },
+    { id: 6, name: 'کاوه' },
+    { id: 7, name: 'شهید چمران' },
+    { id: 8, name: 'شهید باهنر' },
+    { id: 9, name: 'شهدا' },
+    { id: 10, name: 'تختی' },
+    { id: 11, name: 'امام حسین' },
+    { id: 12, name: 'انقلاب' },
+    { id: 13, name: 'سی و سه پل' },
+    { id: 14, name: 'دکتر شریعتی' },
+    { id: 15, name: 'آزادی' },
+    { id: 16, name: 'دانشگاه' },
+    { id: 17, name: 'کارگر' },
+    { id: 18, name: 'کوی امام' },
+    { id: 19, name: 'دفاع مقدس' },
+    { id: 20, name: 'صفه' },
+];
