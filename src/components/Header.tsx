161d64507a7511a7ef3d50874e96265fd5c9f9d1
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-10-18 02:25:00 +1030

    App Imoroved

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,9 @@ export default function Header({ time, date }: { time: string, date: string }) {
     <header className="text-center min-h-fit my-2 font-vazir">
         <h1 className="text-4xl font-bold text-cyan-400">برنامه حرکت قطار شهری اصفهان</h1>
         <p className="text-gray-400 mt-2">ساعت حرکت قطارهای بعدی از ایستگاه مورد نظر شما</p>
-        <span className="">{date}</span>
+        <span className="font-vazir rtl">{date}</span>
         <hr />
-        <span className=''>{time}</span>
+        <span className='font-vazir rtl'>{time}</span>
     </header>
   )
 }
