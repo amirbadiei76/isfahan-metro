e27aa7e65fad014c4392f4fd1a513516768813c7
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-10-19 19:06:16 +1030

    Font changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
 export default function Header({ time, date }: { time: string, date: string }) {
   return (
     <header className="text-center min-h-fit py-2 font-vazir bg-gray-900">
-        <h1 className="text-4xl font-bold text-cyan-400">برنامه حرکت قطار شهری اصفهان</h1>
+        <h1 className="text-3xl font-bold text-cyan-400">برنامه حرکت قطار شهری اصفهان</h1>
         <p className="text-gray-300 mt-2">ساعت حرکت قطارهای بعدی از ایستگاه مورد نظر شما</p>
         <span className="font-vazir rtl text-white">{date}</span>
         <hr />
