56f4b5e44b4fabfb4e33b33fd4bd0e620c8ad7df
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-10-26 05:15:57 +1030

    Header changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,9 @@ export default function Header({ time, date }: { time: string, date: string }) {
   return (
     <header className="text-center min-h-fit py-2 font-vazir bg-gray-900">
         <h1 className="text-3xl font-bold text-cyan-400">برنامه حرکت قطار شهری اصفهان</h1>
-        <p className="text-gray-300 text-sm mt-2">ساعت حرکت قطارهای بعدی از ایستگاه مورد نظر شما</p>
         <div className="flex flex-col gap-1 mt-3">
-          <span className="font-vazir rtl text-white">{date}</span>
-          <span className='font-vazir rtl text-white'>{time}</span>
+          <h2 className="font-vazir rtl text-white">{date}</h2>
+          <h2 className='font-vazir rtl text-white'>{time}</h2>
         </div>
     </header>
   )
