30c1a72ca95edac055a922873560cba3c5a33d6f
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-11-05 00:22:28 +1030

    Metro Style changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 
 export default function Header({ time, date }: { time: string, date: string }) {
   return (
-    <header className="text-center min-h-fit py-2 font-vazir bg-gray-900">
+    <header className="text-center min-h-fit py-2 font-vazir bg-gray-900 mb-1">
         <h1 className="text-3xl font-bold text-cyan-400">برنامه حرکت قطار شهری اصفهان</h1>
-        <div className="flex flex-col gap-1 mt-3">
+        <div className="flex flex-col gap-1">
           <h2 className="font-vazir rtl text-white">{date}</h2>
           <h2 className='font-vazir rtl text-white'>{time}</h2>
         </div>
