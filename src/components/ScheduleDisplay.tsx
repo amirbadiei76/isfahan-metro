746a28e919cbf32dccefda5745def7fd7a01f7f0
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-11-22 22:16:57 +0330

    Other styles changed

--- src/components/ScheduleDisplay.tsx
+++ src/components/ScheduleDisplay.tsx
@@ -20,7 +20,7 @@ export default function ScheduleDisplay ({ dayType, results, directionText, sour
             <div className="flex justify-between items-center mb-4 border-b border-gray-600 pb-3">
             <div>
                 <h2 className="text-xl font-semibold text-cyan-500">{directionText}</h2>
-                { dayType && <p className="text-sm text-blue-600">برنامه امروز ({dayType})</p> }
+                { dayType && <p className="text-sm text-blue-800">برنامه امروز ({dayType})</p> }
             </div>
             </div>
     
