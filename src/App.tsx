185f751e9e3e4ce8803f70546050d5b5105bfe2a
Amirhossain Badiei <amir_badiei@yahoo.com>
2025-12-27 19:28:46 +0330

    App changed

--- src/App.tsx
+++ src/App.tsx
@@ -100,10 +100,10 @@ function App() {
     return (
       <>
         <Header date={date} time={time}  />
-        <main className="bg-gray-900 min-h-(--remain-height) flex flex-col items-center p-4 text-white font-vazir">
+        <main className="bg-gray-900 min-h-(--remain-height) flex flex-col items-center p-5 text-white font-vazir">
             <div className="w-full max-w-2xl mx-auto">
 
-            <div className="bg-gray-800 p-6 rounded-lg shadow-lg rtl">
+            <div className="bg-gray-700 p-6 rounded-lg shadow-lg rtl">
               <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
                 <StationSelect
                   label="ایستگاه مبدا"
