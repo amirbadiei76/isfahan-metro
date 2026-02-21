875cc65aa8398b9a8ad2ec9bf9a5869e0b312112
Amirhossain Badiei <amir_badiei@yahoo.com>
2026-02-21 23:44:06 +0330

    Some texts added

--- src/components/streets/IsfahanStreetTexts.tsx
+++ src/components/streets/IsfahanStreetTexts.tsx
@@ -35,7 +35,7 @@ export default function IsfahanStreetTexts() {
         <text x={0} y={0} fill="black"
           stroke="#F7FABF"
           strokeWidth="2"
-          paintOrder="stroke" fontSize={12} transform="rotate(-90)" className="font-vazir">ارتش</text>
+          paintOrder="stroke" fontSize={11} transform="rotate(-90)" className="font-vazir">ارتش</text>
 
 
       </g>
