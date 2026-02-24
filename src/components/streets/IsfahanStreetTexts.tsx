b8ae22764cd0a19e2643fcb1b97900e09e6ab6f6
Amirhossain Badiei <amir_badiei@yahoo.com>
2026-02-24 22:20:56 +0330

    Some texts added

--- src/components/streets/IsfahanStreetTexts.tsx
+++ src/components/streets/IsfahanStreetTexts.tsx
@@ -3,10 +3,10 @@
 export default function IsfahanStreetTexts() {
   return (
     <g id="IsfahanStreets texts" transform="translate(200, 0)">
-      <text x={-245} y={683} fill="#000"
+      <text x={244} y={683} fill="#000"
         stroke="#F9B29C"
         strokeWidth="2"
-        paintOrder="stroke" transform="rotate(-40)" fontSize={11} className="font-vazir">بزرگراه شهید کشوری</text>
+        paintOrder="stroke" transform="rotate(-40 244 683)" fontSize={11} className="font-vazir">بزرگراه شهید کشوری</text>
         
       <text x={-370} y={590} fill="#000"
         stroke="#F9B29C"
@@ -17,6 +17,11 @@ export default function IsfahanStreetTexts() {
         stroke="#F9B29C"
         strokeWidth="2"
         paintOrder="stroke" transform="rotate(2 -150 625)" fontSize={11} className="font-vazir">بزرگراه شهید عقارب پرست</text>
+        
+      <text x={-500} y={649} fill="#000"
+        stroke="#F9B29C"
+        strokeWidth="2"
+        paintOrder="stroke" transform="rotate(-46 -500 649)" fontSize={11} className="font-vazir">بزرگراه شهید کاظمی</text>
 
         
       {/* stroke: #FCD6A4 */}
@@ -34,28 +39,33 @@ export default function IsfahanStreetTexts() {
         stroke="#FCD6A4"
         strokeWidth="2"
         paintOrder="stroke" fontSize={11} className="font-vazir">کشاورز</text>
+        
+      <text x={-520} y={750} fill="#000"
+        stroke="#FCD6A4"
+        strokeWidth="2"
+        paintOrder="stroke" transform="rotate(-73 -520 750)" fontSize={11} className="font-vazir">الغدیر</text>
+
 
       {/* stroke: #F7FABF */}
-      <text x={-575} y={-171} fill="#000"
+      <text x={-175} y={571} fill="#000"
         stroke="#F7FABF"
         strokeWidth="2"
-        paintOrder="stroke" fontSize={11} transform="rotate(-90)" className="font-vazir">جانبازان</text>
+        paintOrder="stroke" fontSize={11} transform="rotate(-90 -175 571)" className="font-vazir">جانبازان</text>
 
-      <text x={-575} y={60} fill="#000"
+      <text x={-10} y={570} fill="#000"
         stroke="#F7FABF"
         strokeWidth="2"
-        paintOrder="stroke" fontSize={11} transform="rotate(-83)" className="font-vazir">شهدای صفه</text>
+        paintOrder="stroke" fontSize={11} transform="rotate(-83 -10 570)" className="font-vazir">شهدای صفه</text>
         
-      <text x={-575}
-            y={60}
-            fill="#000"
-            stroke="#F7FABF"
-            strokeWidth="2"
-            paintOrder="stroke"
-            fontSize={11}
-            transform="rotate(57 -835 890)"
-            className="font-vazir"
-      >گذرگاه شهدای صفه</text>
+      <text x={-7} y={645} fill="#000"
+        stroke="#F7FABF" strokeWidth="2"
+        paintOrder="stroke" fontSize={11} transform="rotate(57 -7 645)" className="font-vazir" >گذرگاه شهدای صفه</text>
+
+      
+      <text x={-420} y={418} fill="#000"
+        stroke="#F7FABF" strokeWidth="2"
+        paintOrder="stroke" fontSize={11} transform="rotate(21 -420 418)" className="font-vazir" >قائمیه</text>
+
 
 
     </g>
